ce53085492d6a459842721b892581c2c4c7ac178
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-13 17:40:30 +0400

    added nav component

---
+++ components/Navigation/index.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { motion, useCycle } from 'framer-motion';
+import { NavList } from './NavList';
+import { Hamburger } from './Hamburger';
+import { useState } from 'react';
+
+const NavWrapper = styled(motion.nav)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  text-align: center;
+`;
+
+const HamburgerButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  z-index: 2;
+`;
+
+const Thing = styled(motion.div)`
+  background-color: #6b5c7b;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const variants = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: () => ({
+    clipPath: 'circle(30px at -40px -40px)',
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  }),
+};
+
+const Navigation = () => {
+  const [isOpen, setOpen] = useCycle(false, true);
+  return (
+    <>
+      <HamburgerButton>
+        <Hamburger isOpen={isOpen} onClick={() => setOpen(!isOpen)} />
+      </HamburgerButton>
+      <NavWrapper animate={isOpen ? 'open' : 'closed'} initial="closed">
+        <Thing variants={variants} />
+        <NavList />
+      </NavWrapper>
+    </>
+  );
+};
+
+export { Navigation };
