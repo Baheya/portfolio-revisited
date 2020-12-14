91b66af39869c9e1980ad844c2a18fa3b4a8fef9
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-14 18:25:59 +0400

    added color function as a util that takes in two colors

--- utils/changeBgColor.js
+++ utils/changeBgColor.js
@@ -1,11 +1,18 @@
-const changeBgColor = () => {
-  const [red, green, blue] = [69, 111, 225];
-  const section1 = document.querySelector('.section1');
+const changeBgColor = (color1, color2) => {
+  let tStart = 100; // Start transition 100px from top
+  let tEnd = 700; // End at 500px
+  let cStart = color1; // Gold
+  let cEnd = color2; // Lime
+  let cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
 
-  document.addEventListener('scroll', () => {
-    let y = 1 + (window.scrollY || window.pageYOffset) / 150;
-    y = y < 1 ? 1 : y; // ensure y is always >= 1 (due to Safari's elastic scroll)
-    const [r, g, b] = [red / y, green / y, blue / y].map(Math.round);
-    section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  });
+  var p = (window.scrollY - tStart) / (tEnd - tStart); // % of transition
+  p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
+  var cBg = [
+    Math.round(cStart[0] + cDiff[0] * p),
+    Math.round(cStart[1] + cDiff[1] * p),
+    Math.round(cStart[2] + cDiff[2] * p),
+  ];
+  return cBg;
 };
+
+export { changeBgColor };
