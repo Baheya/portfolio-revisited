9f55018dec64116eabe1e561962dcd3bf16c532f
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-15 19:21:19 +0400

    added utility that converts rgb string into arrays in order for function to be able to compute color change on scroll

--- utils/changeBgColor.js
+++ utils/changeBgColor.js
@@ -1,8 +1,18 @@
+const convertRGBtoArray = (color1, color2) => {
+  const trimColor1 = color1.replace(/[rgb()]/g, '');
+  const convertedNumberArray1 = trimColor1.split(',').map((string) => +string);
+  const trimColor2 = color2.replace(/[rgb()]/g, '');
+  const convertedNumberArray2 = trimColor2.split(',').map((string) => +string);
+
+  return [convertedNumberArray1, convertedNumberArray2];
+};
+
 const changeBgColor = (color1, color2) => {
+  const convertedColors = convertRGBtoArray(color1, color2);
   let tStart = 100; // Start transition 100px from top
   let tEnd = 700; // End at 500px
-  let cStart = color1; // Gold
-  let cEnd = color2; // Lime
+  let cStart = convertedColors[0]; // Gold
+  let cEnd = convertedColors[1]; // Lime
   let cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
 
   var p = (window.scrollY - tStart) / (tEnd - tStart); // % of transition
