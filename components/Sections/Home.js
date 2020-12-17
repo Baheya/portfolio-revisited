706971fc6fb81ed7673d73f91a4de74de327bd44
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-17 18:35:33 +0400

    replaced home section in index place with home section component

---
+++ components/Sections/Home.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { CollageOne } from '../Collage/CollageOne';
+
+const HomeGrid = styled.section`
+  grid-column: 2 / -2;
+  grid-row: 2 / 3;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: repeat(4, 25vh);
+`;
+
+const HomeText = styled.h1`
+  color: var(--color-text-primary);
+  margin: 0;
+  font-size: 96px;
+  max-width: 900px;
+  grid-column: 1 / 2;
+  grid-row: 2 / 4;
+`;
+
+const HomeSection = () => {
+  return (
+    <HomeGrid>
+      <HomeText name="#Home">
+        Hi! My name is Baheya Khalifa and I'm <span id="about"></span>
+      </HomeText>
+      <CollageOne />
+    </HomeGrid>
+  );
+};
+
+export { HomeSection };
